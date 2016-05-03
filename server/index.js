e8dc3067579266a30e40960aa52bce7965a32288
dibosh <dibosh@newscred.com>
2016-05-04 02:49:24 +0600

    Create basic paginated api for getting presentation list    - Use prezis.json file as datasource of the backend    - API call format: /api/presentations/offset/pagesize    - Example api call: /api/presentations/0/5

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
-var express    = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('lodash')
+var _db = require('underscore-db')
+var low = require('lowdb');
+var jsonDataSource = require('./datasource/prezis.json');
 
-var app        = express();
+var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.json({ message: 'Successfully created the basic api!' });
-});
+// Create database
+var db = low();
+db.object = jsonDataSource;
+// Add underscore-db methods to db
+db._.mixin(_db);
+
+router.route('/presentations/:offset/:pagesize')
+  .get(function(req, res) {
+    res.json({
+      total: db('presentations').size(),
+      presentations: db('presentations').chunk(req.params.pagesize)[req.params.offset] || []
+    });
+  });
 
 app.use('/api', router);
 
