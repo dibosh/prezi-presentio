21bd3488d278c01cb76d313323259fcf32fa478c
dibosh <dibosh@newscred.com>
2016-05-07 16:51:23 +0600

    Make search by title to be case insensitive

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ router.route('/presentation/all')
   .get(function (req, res) {
     var query = req.query.q;
     var querySet = db('presentations').filter(function (presentation) {
-      return _.startsWith(presentation.title, query);
+      return _.startsWith(presentation.title.toLowerCase(), query.toLowerCase());
     });
     res.json({
       total: querySet ? querySet.length : 0,
