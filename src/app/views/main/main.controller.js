9a390df334c2b3308194681c09ba1b5f2276439f
dibosh <dibosh@newscred.com>
2016-05-08 02:26:31 +0600

    Add pagination and order by for presentation grid

--- src/app/views/main/main.controller.js
+++ src/app/views/main/main.controller.js
@@ -8,6 +8,24 @@
   /** @ngInject */
   function MainController(PresentationService, _, $log, $state) {
     var vm = this;
+    vm.orderByOption = {};
+    vm.orderBy = {
+      options: [
+        {
+          name: 'Latest first',
+          value: 'desc'
+        },
+        {
+          name: 'Older first',
+          value: 'asc'
+        }
+      ]
+    };
+
+    vm.visiblePagination = {
+      lower: vm.currentOffset + 1,
+      upper: vm.currentOffset + 1 + vm.pageSize
+    };
 
     function _setupPresentationTiles(presentations) {
       vm.presentationTiles = _.map(presentations, function (presentation) {
@@ -16,10 +34,18 @@
         };
         return presentation;
       });
+      vm.orderByOption = vm.orderBy.options[0].value;
+      vm.orderPresentationsBy(vm.orderByOption);
     }
 
     var onSuccess = function (response) {
       vm.shouldShowProgressView = false;
+      vm.totalPresentations = response.total;
+      vm.visiblePagination.lower = (vm.currentOffset * vm.pageSize) + 1;
+      vm.visiblePagination.upper = vm.visiblePagination.lower + vm.pageSize - 1;
+      if (!vm.maxOffset) {
+        vm.maxOffset = Math.ceil(vm.totalPresentations / vm.pageSize) - 1;
+      }
       _setupPresentationTiles(response.presentations);
     };
 
@@ -29,23 +55,43 @@
       $log.debug(response);
     };
 
-    function _init_() {
+    function _init_(offset) {
       vm.query = '';
       vm.errorMessage = '';
       vm.shouldShowProgressView = true;
       vm.searchPlaceholder = 'Search all presentations';
-      vm.currentOffset = 0;
-      vm.pageSize = 10;
+      vm.currentOffset = offset;
+      vm.pageSize = 20;
+      vm.totalPresentations = 0;
 
       PresentationService.getPresentations(vm.currentOffset, vm.pageSize)
         .success(onSuccess)
         .error(onFailure);
     }
 
+    vm.fetchNextPage = function () {
+      vm.currentOffset += 1;
+      _init_(vm.currentOffset);
+    };
+
+    vm.fetchPrevPage = function () {
+      vm.currentOffset -= 1;
+      vm.currentOffset = vm.currentOffset < 0 ? 0 : vm.currentOffset;
+      _init_(vm.currentOffset);
+    };
+
     vm.searchByTitle = function () {
       $state.go('search', {query: vm.query});
     };
 
-    _init_();
+    vm.orderPresentationsBy = function (orderByOption) {
+      var objectsWithDateFromString = _.map(vm.presentationTiles, function (presentation) {
+        presentation['createdAtDateObj'] = Date.parse(presentation['createdAt']);
+        return presentation;
+      });
+      vm.presentationTiles = _.orderBy(objectsWithDateFromString, ['createdAtDateObj'], [orderByOption]);
+    };
+
+    _init_(0);
   }
 })();
